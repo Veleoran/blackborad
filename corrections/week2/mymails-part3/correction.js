c8e8b508c65ded3e65437c853a90eb8e207e2fef
Julie Bricka <bricka.j@gmail.com>
2022-04-20 11:27:54 +0200

    add mymail-part3

---
+++ corrections/week2/mymails-part3/correction.js
@@ -0,0 +1,116 @@
+// PART 2
+
+/* const newMessage = ` 
+  <div class="row new-row">
+  <img class="avatar" src="/images/avatar-1.jpg" />
+  <div class="text-container">
+    <h6>John Doe</h6>
+    <p>New message</p>
+  </div>
+  <span class="delete">&#x2716;</span>
+  </div>
+  `;
+
+document.querySelector('#msg-container').innerHTML += newMessage
+
+const messagesCount = document.querySelectorAll('p').length
+document.querySelector('#count').textContent = messagesCount; */
+
+let year = new Date().getUTCFullYear();
+let month;
+let day;
+
+if (new Date().getMonth() < 9) {
+    month = "0" + (new Date().getMonth() + 1)
+} else {
+    month = new Date().getMonth() + 1
+}
+
+if (new Date().getDate() < 9) {
+    day = "0" + new Date().getDate()
+} else {
+    day = new Date().getDate()
+}
+
+const date = year + "-" + month + "-" + day
+document.querySelector('#footer').innerHTML += `<span id="date">${date}</span>`
+
+
+// 1 - Supprimer un message avec le deleteButton
+for (let i = 0; i < document.querySelectorAll('.delete').length; i++) {
+    document.querySelectorAll('.delete')[i].addEventListener("click",
+        function () {
+            this.parentNode.remove();
+            const messagesCount = document.querySelectorAll('p').length;
+            document.querySelector('#count').textContent = messagesCount;
+        }
+    )
+}
+
+// 2 - Ajouter un message avec l'input
+document.querySelector('#btn-add').addEventListener("click",
+    function () {
+
+        const message = document.getElementById('add-message').value;
+
+        console.log(message)
+
+        document.querySelector('#msg-container').innerHTML = document.querySelector('#msg-container').innerHTML + ` 
+        <div class="row new-row">
+        <img class="avatar" src="avatar-1.jpg" />
+        <div class="text-container">
+          <h6>Noel Paganelli</h6>
+          <p>${message}</p>
+        </div>
+        <span class="delete">&#x2716;</span>
+      </div>
+`;
+        document.querySelector('#add-message').value = "";
+
+        const messagesCount = document.querySelectorAll('p').length;
+        document.querySelector('#count').textContent = messagesCount;
+
+        for (let i = 0; i < document.querySelectorAll('.delete').length; i++) {
+            document.querySelectorAll('.delete')[i].addEventListener("click",
+                function () {
+                    this.parentNode.remove();
+                    const messagesCount = document.querySelectorAll('p').length;
+                    document.querySelector('#count').textContent = messagesCount;
+                }
+            )
+        }
+    }
+)
+
+// 3 - Rechercher un message
+document.querySelector('#btn-search').addEventListener("click",
+    function () {
+
+        const textToCompare = document.querySelector('#search-message').value.toLowerCase();
+
+        for (let i = 0; i < document.querySelectorAll('h6').length; i++) {
+            if (document.querySelectorAll('h6')[i].textContent.toLowerCase().includes(textToCompare) === false) {
+                const textToShow = document.querySelectorAll('h6')[i].parentNode.parentNode;
+                textToShow.style.display = "none";
+            } else {
+                const textToHide = document.querySelectorAll('h6')[i].parentNode.parentNode;
+                textToHide.style.display = "flex";
+            }
+        }
+
+        document.querySelector('#search-message').value = "";
+
+
+
+        /*  for (let i = 0; i < document.querySelectorAll('h6').length; i++) {
+             if (document.querySelectorAll('h6')[i].textContent.toLowerCase().includes(textToCompare) === false) {
+                 const textToShow = document.querySelectorAll('h6')[i].parentNode.parentNode;
+                 textToShow.style.display = "none";
+             } else {
+                 const textToHide = document.getElementsByTagName('h6')[i].parentNode.parentNode;
+                 textToHide.style.display = "flex";
+             }
+         }
+         document.querySelector('#search-message').value = ""; */
+    }
+); 
---
+++ tests/week2/mymails-part3/app.test.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const path = require('path');
+const { JSDOM } = require("jsdom");
+
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+const js = fs.readFileSync(path.resolve(__dirname, './correction.js'), 'utf8');
+
+let virtualPage = new JSDOM(html, { runScripts: "dangerously" });
+const documentJS = virtualPage.window.document;
+
+const script = documentJS.createElement('script');
+script.textContent = js;
+documentJS.body.appendChild(script);
+
+it('Check retrieve msg in add-message input', () => {
+    const allMessages = documentJS.querySelectorAll('.row').length;
+    documentJS.querySelector('#add-message').value = "Hello";
+
+    const element = documentJS.querySelector('#btn-add');
+    const event = documentJS.createEvent('MouseEvents');
+    event.initMouseEvent('click');
+    element.dispatchEvent(event);
+
+    expect(documentJS.querySelectorAll('p')[allMessages].textContent).toBe("Hello");
+});
+
+it('Check add message button + update counter + clean input', () => {
+    const allMessages = documentJS.querySelectorAll('.row').length;
+    const element = documentJS.querySelector('#btn-add');
+
+    const event = documentJS.createEvent('MouseEvents');
+    event.initMouseEvent('click');
+    element.dispatchEvent(event);
+
+    expect(documentJS.querySelectorAll('.row').length).toBe(allMessages + 1);
+    expect(documentJS.querySelector('#add-message').textContent).toBe('');
+    expect(documentJS.querySelector('#count').textContent).toBe(JSON.stringify(allMessages + 1));
+});
+
+it('Check search message button + retrieve msg + search message + clean input', () => {
+    documentJS.querySelector('#search-message').value = "Alexandra";
+    const textToCompare = documentJS.querySelector('#search-message').value.toLowerCase();
+
+    const element = documentJS.querySelector('#btn-search');
+    const event = documentJS.createEvent('MouseEvents');
+    event.initMouseEvent('click');
+    element.dispatchEvent(event);
+
+    for (let i = 0; i < documentJS.querySelectorAll('h6').length; i++) {
+        if (documentJS.querySelectorAll('h6')[i].textContent.toLowerCase().includes(textToCompare) === false) {
+            const textToShow = documentJS.querySelectorAll('.row')[i];
+            expect(textToShow.style.display).toBe("none");
+        } else {
+            const textToHide = documentJS.querySelectorAll('.row')[i];
+            expect(textToHide.style.display).toBe("flex");
+        }
+    }
+
+    documentJS.querySelector('#search-message').value = "";
+});
+
+it('Check delete message button + update counter', () => {
+    const elems = documentJS.querySelectorAll('.delete');
+
+    for (let i = 0; i < elems.length; i++) {
+        const event = documentJS.createEvent('MouseEvents');
+        event.initMouseEvent('click');
+        elems[i].dispatchEvent(event);
+    }
+    expect(documentJS.querySelectorAll('.delete').length).toBe(0);
+    expect(documentJS.querySelector('#count').textContent).toBe("0");
+});
