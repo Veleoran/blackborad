d00135b4f24937a270ef576b0875cd01bb1103ce
Antoine <antoinedury.pro@gmail.com>
2022-04-21 13:46:34 +0200

    Add Jest to dev dependencies

---
+++ corrections/week2/blackboard-part2/models/setup.js
@@ -0,0 +1,21 @@
+// You should close this file and not modify it 😉
+
+const mongoose = require('mongoose');
+const connectionString = require('../connection');
+
+mongoose.connect(connectionString, {
+	connectTimeoutMS: 2000,
+}).catch(error => console.error(error));
+
+const articleSchema = mongoose.Schema({
+	name: String,
+	description: String,
+	price: Number,
+	stock: Number,
+	weight: Number,
+	image: String,
+});
+
+const Article = mongoose.model('articles', articleSchema);
+
+module.exports = Article;
