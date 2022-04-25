69318b3b495f751b96c7ed095a726581d0c9d101
Antoine <antoinedury.pro@gmail.com>
2022-04-25 20:10:15 +0200

    Yellow submarine validation

--- tests/week1/yellowsubmarine/app.test.js
+++ tests/week1/yellowsubmarine/app.test.js
@@ -7,56 +7,53 @@ const css = fs.readFileSync(path.resolve(__dirname, './style.css'), 'utf8');
 
 const virtualPage = new JSDOM(html);
 const document = virtualPage.window.document;
-
 document.head.innerHTML = `<style>${css}</style>`;
 
 function getSelector(selector) {
-    const cssRules = document.styleSheets.item(0).cssRules;
-    const result = { isExist: false, style: null };
-    for (let i = 0; i < cssRules.length; i++) {
-        if (cssRules[i].selectorText == selector) {
-            result.isExist = true;
-            result.style = cssRules[i].style;
-        }
+  const cssRules = document.styleSheets.item(0).cssRules;
+  const result = { isExist: false, style: null };
+  for (let i = 0; i < cssRules.length; i++) {
+    if (cssRules[i].selectorText == selector) {
+      result.isExist = true;
+      result.style = cssRules[i].style;
     }
-    return result;
+  }
+  return result;
 }
 
-// HEADER
-it("Check if header and if contain an image", () => {
-    expect(document.querySelector('#header')).toBeTruthy();
-    const elements = document.querySelector('#header').children
-    let elementsArray = [];
-    let imageSrc = "";
-    for (const e of elements) {
-        elementsArray.push(e.tagName)
-        if (e.tagName === "IMG") {
-            imageSrc = e.src
-        }
+// Header
+it('Header contains image', () => {
+  expect(document.querySelector('#header')).toBeTruthy();
+  const elements = document.querySelector('#header').children;
+  let elementsArray = [];
+  let imageSrc = '';
+  for (const e of elements) {
+    elementsArray.push(e.tagName);
+    if (e.tagName === 'IMG') {
+      imageSrc = e.src;
     }
-    expect(imageSrc).toBe('images/beatles-header.jpg');
-    expect(elementsArray.includes('IMG')).toBe(true);
+  }
+
+  expect(imageSrc).toBe('images/beatles-header.jpg');
+  expect(elementsArray.includes('IMG')).toBe(true);
 });
 
-// SINGER CONTAINER
-it("Check number of singer_container and their children", () => {
-    expect(document.querySelectorAll('.singer_container').length).toBe(4);
-    const elements = document.querySelector('.singer_container').children
-    let elementsArray = [];
-    for (const e of elements) {
-        elementsArray.push(e.tagName)
-    }
-    expect(elementsArray.includes('P')).toBe(true);
-    expect(elementsArray.includes('IMG')).toBe(true);
+// Singer container
+it('Number of singer_container and children', () => {
+  expect(document.querySelectorAll('.singer_container').length).toBe(4);
+  const elements = document.querySelector('.singer_container').children;
+  let elementsArray = [];
+  for (const e of elements) {
+    elementsArray.push(e.tagName);
+  }
+
+  expect(elementsArray.includes('P')).toBe(true);
+  expect(elementsArray.includes('IMG')).toBe(true);
 });
 
-it("Check if singer container has flexbox", () => {
-    const selector = getSelector('.singer_container');
-    expect(selector.style.display).toBe('flex');
-    expect(selector.style).toEqual(expect.objectContaining({ 'flex-direction': 'column' }));
-    expect(selector.style).toEqual(expect.objectContaining({ 'align-items': 'center' }));
+it('Singer container has flexbox', () => {
+  const selector = getSelector('.singer_container');
+  expect(selector.style.display).toBe('flex');
+  expect(selector.style).toEqual(expect.objectContaining({ 'flex-direction': 'column' }));
+  expect(selector.style).toEqual(expect.objectContaining({ 'align-items': 'center' }));
 });
-
-
-
-
