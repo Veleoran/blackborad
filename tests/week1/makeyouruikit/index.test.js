aa8381b641d77bd158fdd05a322b2b6dcfb6a530
Antoine <antoinedury.pro@gmail.com>
2022-04-25 19:11:39 +0200

    Make your UI kit challenge validation

--- tests/week1/makeyouruikit/index.test.js
+++ tests/week1/makeyouruikit/index.test.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
 const path = require('path');
-const { JSDOM } = require("jsdom");
+const { JSDOM } = require('jsdom');
 
 const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
 const css = fs.readFileSync(path.resolve(__dirname, './style.css'), 'utf8');
 
 const virtualPage = new JSDOM(html);
 const document = virtualPage.window.document;
-
 document.head.innerHTML = `<style>${css}</style>`;
 
 function getSelector(selector) {
@@ -22,20 +21,18 @@ function getSelector(selector) {
     return result;
 }
 
-console.log(getSelector('.card-element').style)
-
-// COLORS
-it("Ckeck if square", () => {
+// Color squares
+it('Color squares presence', () => {
     expect(document.querySelectorAll('.color').length).toBe(5);
 });
 
-it("Color square size", () => {
+it('Color squares size', () => {
     const selector = getSelector('.color');
     expect(selector.style.height).toBe('85px');
     expect(selector.style.width).toBe('85px');
 });
 
-it("Color square colors", () => {
+it('Color squares background colors', () => {
     expect(getSelector('.darkgrey').style).toEqual(expect.objectContaining({ 'background-color': '#212529' }));
     expect(getSelector('.grey').style).toEqual(expect.objectContaining({ 'background-color': '#CED4DA' }));
     expect(getSelector('.darkblue').style).toEqual(expect.objectContaining({ 'background-color': '#03045E' }));
@@ -43,12 +40,12 @@ it("Color square colors", () => {
     expect(getSelector('.lightblue').style).toEqual(expect.objectContaining({ 'background-color': '#CAF0F8' }));
 });
 
-//BUTTONS
-it("Check if button", () => {
+// Button
+it('Button presence', () => {
     expect(document.querySelector('.btn')).toBeTruthy();
 });
 
-it("Check button css", () => {
+it('Button CSS', () => {
     const selector = getSelector('.btn');
     expect(selector.style.width).toBe('120px');
     expect(selector.style.cursor).toBe('pointer');
@@ -56,48 +53,46 @@ it("Check button css", () => {
     expect(selector.style).toEqual(expect.objectContaining({ 'background-color': '#03045E' }));
 });
 
-it("Check button css hover", () => {
+it('Button CSS hover', () => {
     const selector = getSelector('.btn:hover');
     expect(selector.style).toEqual(expect.objectContaining({ 'background-color': '#023E8A' }));
 });
 
-// TABLES
-it("Check if table", () => {
+// Table
+it('Table presence', () => {
     expect(document.querySelector('table')).toBeTruthy();
 });
 
-it("Check table css", () => {
+it('Table CSS', () => {
     const selector = getSelector('table');
     expect(selector.style.border).toBe('1px solid #DFDFDF');
     expect(selector.style).toEqual(expect.objectContaining({ 'border-radius': '4px' }));
 });
 
-// TYPOGRAPHY
-it("Check if typo span", () => {
+// Typography
+it('Typography spans', () => {
     expect(document.querySelector('.bold')).toBeTruthy();
     expect(document.querySelector('.regular')).toBeTruthy();
 });
 
-it("Check if typo in body css", () => {
+it('Typography CSS for body', () => {
     expect(getSelector('body').style).toEqual(expect.objectContaining({ 'font-family': "'Noto Sans', sans-serif" }));
 });
 
-it("Check if typo span css", () => {
+it('Typography CSS for span', () => {
     expect(getSelector('.bold').style).toEqual(expect.objectContaining({ 'font-weight': '400' }));
     expect(getSelector('.bold').style).toEqual(expect.objectContaining({ 'font-size': '20px' }));
     expect(getSelector('.regular').style).toEqual(expect.objectContaining({ 'font-weight': '200' }));
     expect(getSelector('.regular').style).toEqual(expect.objectContaining({ 'font-size': '20px' }));
 });
 
-// CARDS
-it("Check if card exists", () => {
+// Card
+it('Card presence', () => {
     expect(document.querySelector('.card-element')).toBeTruthy();
 });
 
-it("Check card css", () => {
+it('Card CSS', () => {
     const selector = getSelector('.card-element');
     expect(selector.style.border).toBe('1px solid #DFDFDF');
     expect(selector.style).toEqual(expect.objectContaining({ 'border-radius': '4px' }));
 });
-
-
