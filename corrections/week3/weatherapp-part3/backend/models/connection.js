a286a15bc30b9c8216ab41f21507ab07da01247e
Antoine <antoinedury.pro@gmail.com>
2022-05-03 13:15:16 +0200

    Remove connection string

--- corrections/week3/weatherapp-part3/backend/models/connection.js
+++ corrections/week3/weatherapp-part3/backend/models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://admin:4hTpCrYPvahy5IUq@cluster0.r9ouk.mongodb.net/weatherapp';
+const connectionString = 'mongodb+srv://.../weatherapp';
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .catch(error => console.error(error))
