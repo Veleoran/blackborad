1f6f885a11e3292079543cd8718899d1c25d56b6
Julie Bricka <bricka.j@gmail.com>
2022-05-04 15:28:05 +0200

    add orientexpress examples

--- week3/orientexpress/routes/index.js
+++ week3/orientexpress/routes/index.js
@@ -3,6 +3,63 @@ var router = express.Router();
 
 let trips = [{ departure: 'Paris', arrival: 'Lyon' }, { departure: 'Lyon', arrival: 'Marseille' }];
 
-// Insert your code here
+// POST /trips - Sample result: 
+
+/*{
+  "allTrips": [
+    {
+      "departure": "Paris",
+      "arrival": "Lyon"
+    },
+    {
+      "departure": "Lyon",
+      "arrival": "Marseille"
+    },
+    {
+      "departure": "Grenoble",
+      "arrival": "Strasbourg"
+    }
+  ]
+} */
+
+// GET /trips - Sample result:
+
+/* 
+{
+  "allTrips": [
+    {
+      "departure": "Paris",
+      "arrival": "Lyon"
+    },
+    {
+      "departure": "Lyon",
+      "arrival": "Marseille"
+    },
+    {
+      "departure": "Grenoble",
+      "arrival": "Strasbourg"
+    }
+  ]
+} 
+*/
+
+// GET /lastTrip - Sample result:
+
+/* 
+{
+ "lastTrip": {
+   "departure": "Grenoble",
+   "arrival": "Strasbourg"
+ }
+} 
+*/
+
+// DELETE /trips - Sample result:
+
+/*
+{
+    "allTrips": []
+} 
+*/
 
 module.exports = router;
