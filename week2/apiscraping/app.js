34ebf2068dc324059c8a63d3269242bb53c53dd1
Anais Moutalier <anaismoutarlier@gmail.com>
2022-05-19 16:22:06 +0200

    create skeleton

--- week2/apiscraping/app.js
+++
@@ -1,3 +0,0 @@
-const db = require('./database/setup'); // Do not edit/remove code this line
-
-// Insert your code here
--- week3/deploymetonetlify/urls.js
+++
@@ -1,4 +0,0 @@
-// Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
-
-module.exports = { FRONTEND_URL }; // Do not edit/remove this line
