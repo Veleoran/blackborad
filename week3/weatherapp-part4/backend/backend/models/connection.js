aeaa966f2c646a76fb1431e1c6bf646d860da61f
Anais Moutalier <anaismoutarlier@gmail.com>
2022-05-19 17:01:24 +0200

    reset resource weatherapp-part4

--- week3/weatherapp-part4/backend/backend/models/connection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectionString = 'mongodb+srv://.../weatherapp';
-
-mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
-  .catch(error => console.error(error))
-  .then(() => console.log('Database connected'));
