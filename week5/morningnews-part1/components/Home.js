acf32499f2da95ed2d71b8b4b85b0355e53597cd
Anais Moutarlier <anaismoutarlier@gmail.com>
2022-06-24 14:43:59 +0200

    Mise à jour de la ressource morningnews-part1

---
+++ week5/morningnews-part1/components/Home.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Home.module.css';
+
+function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.background}>HOME</div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default Home;
