29baf56a40607b2827422e6fec8dc416a4e34d45
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-03-20 16:07:08 +0100

    Mise à jour de la ressource myfirstproject

---
+++ week1/myfirstproject/app.js
@@ -0,0 +1,11 @@
+function getPerfectNumber() {
+  // Change this variable with the number given in the instructions to validate the project
+  const perfectNumber = 1407;
+
+  return perfectNumber;
+}
+
+console.log(getPerfectNumber());
+
+// Do not edit/remove code under this line
+module.exports = getPerfectNumber;
