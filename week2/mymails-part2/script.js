e5b8a001dc999744f4b8c41dfa67f0a2f3a9fc41
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-03-27 14:37:39 +0200

    1

--- week2/mymails-part2/script.js
+++ week2/mymails-part2/script.js
@@ -3,12 +3,13 @@ document.querySelector('#msg-container').innerHTML += `<div class ="row rew-row"
 <img class="avatar" src="images/avatar-1.jpg" />
 <div class="text-container">
 <h6>John H</h6>
-<p>On peut créer un sélecteur ciblant les éléments d'un type donné appartenant à une classe donnée. Dans l'exemple suivant, la classe highlight met en surbrillance, mais elle le fait différemment quand elle s'applique à un <span> ou à un titre <h1>. Nous le faisons en utilisant le sélecteur de type pour l'élément ciblé, avec la classe ajoutée, sans espace blanc entre les deux.
+<p> bonjour
 </p>
 </div>
 <span class="delete">&#x2716;</span>
 </div>
 `;
+
 const messagesCount = document.querySelectorAll('p').length
 
 document.querySelector('#count') . textContent = messagesCount
