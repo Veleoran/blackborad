e7000d42254d364e1047bcf9e21319eae9aad102
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-03-28 20:53:40 +0200

    z

--- week2/mymails-part3/script.js
+++ week2/mymails-part3/script.js
@@ -33,51 +33,45 @@ if (new Date().getDate() < 9) {
 const date = year + "-" + month + "-" + day;
 document.querySelector('#footer').innerHTML += `<span id="date">${date}</span>`;
 
-
-
 for (let i = 0; i < document.querySelectorAll(".delete").length; i++) {
   document.querySelectorAll(".delete")[i].addEventListener("click", function () {
-    
-
- this.parentNode.remove();
- const NewmessageCount = document.querySelectorAll('p').length;
- document.querySelector('#count').textContent = NewmessageCount;
-   
-  }
-);
+    this.parentNode.remove();
+    const newMessageCount = document.querySelectorAll('p').length;
+    document.querySelector('#count').textContent = newMessageCount;
+  });
 }
 
 document.querySelector("#btn-add").addEventListener("click", function () {
   console.log("Click detected!");
   const messageInput = document.querySelector("#add-message");
   const message = messageInput.value;
-  document.querySelector('#msg-container').innerHTML += `
- <div class="row new-row">
-   <img class="avatar" src="avatar-1.jpg" />
-   <div class="text-container">
-     <h6>John Doe</h6>
-     <p>${message}</p>
-   </div>
-   <span class="delete">&#x2716;</span>
- </div>
-`;
+  if (message.trim() !== '') {
+    document.querySelector('#msg-container').innerHTML += `
+      <div class="row new-row">
+        <img class="avatar" src="avatar-1.jpg" />
+        <div class="text-container">
+          <h6>John Doe</h6>
+          <p>${message}</p>
+        </div>
+        <span class="delete">&#x2716;</span>
+      </div>
+    `;
 
-messageInput.value = '';
+    messageInput.value = '';
 
+    const messagesCount = document.querySelectorAll('p').length;
+    document.querySelector('#count').textContent = messagesCount;
 
-const messagesCount = document.querySelectorAll('p').length;
-document.querySelector('#count').textContent = messagesCount;
-
-const deleteButtons = document.querySelectorAll(".delete");
-deleteButtons[deleteButtons.length - 1].addEventListener("click", function () {
-    this.parentNode.remove();
-    const newMessagesCount = document.querySelectorAll('p').length;
-    document.querySelector('#count').textContent = newMessagesCount;
+    const deleteButtons = document.querySelectorAll(".delete");
+    deleteButtons[deleteButtons.length - 1].addEventListener("click", function () {
+      this.parentNode.remove();
+      const newMessagesCount = document.querySelectorAll('p').length;
+      document.querySelector('#count').textContent = newMessagesCount;
+    });
+  }
 });
 
 
-
-});
 
 
 
