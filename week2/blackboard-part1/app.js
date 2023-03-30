f55ca042e8b3eaaf9bab8750d5cef6f7c5f6e9e3
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-03-30 15:10:57 +0200

    Mise à jour de la ressource blackboard-part1

---
+++ week2/blackboard-part1/app.js
@@ -0,0 +1,30 @@
+const db = require('./database/setup'); // Do not edit/remove this line
+
+
+function displayAllArticles() {}
+
+
+function displayArticleByName(articleName) {}
+
+
+function displayArticleByID(articleId) {}
+
+
+function updateArticlePrice(articleId, newPrice) {}
+
+
+function updateArticleStock(articleId, newStock) {}
+
+
+function resetStocks() {}
+
+
+// Do not edit/remove code under this line
+module.exports = {
+	displayAllArticles,
+	displayArticleByName,
+	displayArticleByID,
+	updateArticlePrice,
+	updateArticleStock,
+	resetStocks,
+};
