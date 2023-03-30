b7ab7f6d3c4097cf7982d088bbce8cd4d1cd0e4c
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-03-30 16:44:02 +0200

    e

--- week2/blackboard-part1/app.js
+++ week2/blackboard-part1/app.js
@@ -1,22 +1,76 @@
 const db = require('./database/setup'); // Do not edit/remove this line
 
 
-function displayAllArticles() {}
+function displayAllArticles() {
+	db.find().then(data => {
+		console.log(data);
+	   });
+}
+// displayAllArticles()
 
 
-function displayArticleByName(articleName) {}
+function displayArticleByName(articleName) {
+	db.findOne({
+		name: articleName })
+		.then(data => {
+			console.log(data);
+		})
+	}
+
+// displayArticleByName("Weebot")	
 
 
-function displayArticleByID(articleId) {}
+function displayArticleByID(articleId) {
+	db.findById(articleId)
+		.then(data => {
+			console.log(data);
+		})
+}
+// displayArticleByID('625ea9ca38d6a37769946520')
 
 
-function updateArticlePrice(articleId, newPrice) {}
+function updateArticlePrice(articleId, newPrice) {
+	db.updateOne( 
+		{ _id: articleId },
+		{price: newPrice }
+	).then(() => {
+		db.find().then(data =>
+    {
+		console.log(data);
+	});
+
+ });
+}
+
+// updateArticlePrice('625ea9ca38d6a37769946520', 15900 )
+
+function updateArticleStock(articleId, newStock) {
+	db.updateOne( 
+		{ _id: articleId },
+		{stock: newStock }
+	).then(() => {
+		db.find().then(data =>
+    {
+		console.log(data);
+	});
+
+ });
+}
+
+// updateArticleStock('625ea9ca38d6a37769946520', 80)
 
 
-function updateArticleStock(articleId, newStock) {}
+function resetStocks() {
+	db.updateMany(
+		{},
+		{stock: 0}
+		).then(data => {
+			console.log(data);
 
+		});
+};
 
-function resetStocks() {}
+// resetStocks()
 
 
 // Do not edit/remove code under this line
--- week2/blackboard-part1/connection.js
+++ week2/blackboard-part1/connection.js
@@ -1,4 +1,4 @@
 // Insert your connection string inside this variable
-const connectionString = 'mongodb+srv://.../blackboard';
+const connectionString = 'mongodb+srv://Veldoran:Aju6QhdXyRsGtPv@cluster0.wofhepd.mongodb.net/blackboard';
 
 module.exports = connectionString; // Do not edit/remove this line
