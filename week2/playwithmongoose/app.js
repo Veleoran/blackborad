f196808adc9ce36d1e46ff5387262aec832216fd
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-03-30 15:08:57 +0200

    z

--- week2/playwithmongoose/app.js
+++ week2/playwithmongoose/app.js
@@ -2,15 +2,56 @@ const db = require('./database/setup'); // Do not edit/remove this line
 
 
 // Create todo with name and priority (done = false by default)
-function createTodo(name, priority) {}
+function createTodo(name, priority) {
+    const newtodo = new db({
+        name: name,
+        priority: priority,
+        done: false
+    });
+       
+    newtodo.save().then(() => {
+        db.find().then(data => {
+          console.log(data);
+        });
+              
+    });
+}
+       
+       
+createTodo("courses", 2)
+
 
 
 // Complete one todo by name (update property done = true)
-function completeTodo(name) {}
+function completeTodo(name) {
+    db.updateOne(
+        { name: name },
+        { done: true }
+       ).then(() => {
+        
+        db.find().then(data => {
+          console.log(data);
+        });
+       
+       });
+}
+
+completeTodo('Pay taxes')
+
+
 
 
 // Delete one todo by name
-function deleteTodo(name) {}
+function deleteTodo(name) {
+ db.deleteOne({ name: name}).then(() => {
+    db.find().then(data => {
+        console.log(data)
+
+    })
+ })  
+}
+
+deleteTodo('Walk the dog')
 
 
 module.exports = { createTodo, completeTodo, deleteTodo }; // Do not edit/remove this line
--- week2/playwithmongoose/connection.js
+++ week2/playwithmongoose/connection.js
@@ -1,4 +1,4 @@
 // Insert your connection string inside this variable
-const connectionString = 'mongodb+srv://.../playwithmongoose';
+const connectionString = 'mongodb+srv://Veldoran:Aju6QhdXyRsGtPv@cluster0.wofhepd.mongodb.net/playwithmongoose';
 
 module.exports = connectionString; // Do not edit/remove this line
