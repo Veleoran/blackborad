e85656c2e9cde720a38cb475aed6d78a7da2b389
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-03-31 15:22:39 +0200

    e

--- week2/evaluation-week2/script.js
+++ week2/evaluation-week2/script.js
@@ -1,1 +1,15 @@
 // Write your code here
+ 
+
+document.querySelector('#fetch-btn').addEventListener('click',
+  function () {
+    fetch(`https://backend-omega-seven.vercel.app/api/getjoke`)
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector('#result-question').textContent = data[0].question;
+        document.querySelector('#result-punchline').textContent =data[0].punchline;
+    }) 
+  })
+
+
+//   document.querySelector('#result-question').textContent =data[0].question;
