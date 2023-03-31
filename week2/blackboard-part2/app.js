c386cb1f6431bc59ac712987c38e151a05935236
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-03-31 11:52:23 +0200

    Mise à jour de la ressource blackboard-part2

---
+++ week2/blackboard-part2/app.js
@@ -0,0 +1,77 @@
+// Add your own requires here 😉
+
+/*
+** Articles
+*/
+
+function displayAllArticles() {
+	Article.find().then(data => {
+		console.log('ARTICLES =>', data);
+	});
+}
+
+function displayArticleByName(articleName) {
+	Article.findOne({ name: articleName }).then(data => {
+		console.log('ARTICLE =>', data);
+	});
+}
+
+function displayArticleByID(articleId) {
+	Article.findById(articleId).then(data => {
+		console.log('ARTICLE =>', data);
+	});
+}
+
+function updateArticlePrice(articleId, newPrice) {
+	Article.updateOne({ _id: articleId }, { price: newPrice }).then(() => {
+		console.log(`Price updated for ${articleId}`);
+	});
+}
+
+function updateArticleStock(articleId, newStock) {
+	Article.updateOne({ _id: articleId }, { stock: newStock }).then(() => {
+		console.log(`Stock updated for ${articleId}`);
+	});
+}
+
+function resetStocks() {
+	Article.updateMany({}, { stock: 0 }).then(() => {
+		console.log(`Stocks successfully reset`);
+	});
+}
+
+
+/*
+** Users
+*/
+
+function displayAllUsers() { }
+
+function deleteUser(userId) { }
+
+
+/*
+** Orders
+*/
+
+function displayAllOrders() { }
+
+function updateOrderPaymentStatus(orderId, isPaid) { }
+
+function deleteOrder(orderId) { }
+
+
+// Do not edit/remove code under this line
+module.exports = {
+	displayAllArticles,
+	displayArticleByName,
+	displayArticleByID,
+	updateArticlePrice,
+	updateArticleStock,
+	resetStocks,
+	displayAllUsers,
+	deleteUser,
+	displayAllOrders,
+	updateOrderPaymentStatus,
+	deleteOrder,
+};
