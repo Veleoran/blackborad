fc47bb0ad10f63f1102d1643d8e170788042eaa7
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-03 12:59:21 +0200

    oldmongoose

--- week3/myoldfriendmongoose/app.js
+++ week3/myoldfriendmongoose/app.js
@@ -3,18 +3,20 @@ const Country = require('./models/countries');
 const City = require('./models/cities');
 
 
-// Create country with name, flag image, currency and population
+ //Create country with name, flag image, currency and population
 function createCountry(name, flagImg, currency, population) { 
+  const newCountry = new Country({
     name: name,
     flagImg: flagImg,
     currency: currency,
     population: population,
+  })
 }
 // Sample call:
-// createCountry('Australia', 'autralia.png', 'AUD', [
-//  { populationNbr: 25400000, year: new Date('2015-08-24') },
-// ]);
-const newCountry = new db({
+createCountry('Australia', 'autralia.png', 'AUD', [
+  { populationNbr: 25400000, year: new Date('2015-08-24') },
+ ]);
+const newCountry = new countries({
     name:'Australia'
     flagImg: 'australia.png',
     currency: 'AUD',
@@ -22,16 +24,34 @@ const newCountry = new db({
     }]);
 
 
-})
+}
 
-// Create city with name, population and country foreign key
-function createCity(name, currentPopulation, countryId) { }
+ Create city with name, population and country foreign key
+function createCity(name, currentPopulation, countryId) {
+    const newCity = new City({
+    name: name,
+    currentPopulation: currentPopulation,
+    countryId: countryId,
+ })
+}
 // Sample call:
-// createCity('Sydney', 5312163, 'COUNTRY_ID_TO_REPLACE');
+ createCity('Sydney', 5312163, 'COUNTRY_ID_TO_REPLACE');
+
+ const newCity = new City({
+    name: 'Sydney',
+    currentPopulation: '5312163',
+    countryId: 'COUNTRY_ID_TO_REPLACE'
+ });
 
 
 // Display country population from country name
-function displayCountryPopulation(countryName) { }
+function displayCountryPopulation(countryName) {
+    Population.findOne({ countryName: 'Australia'})
+    .then(data => {
+        console.log(data);
+    })
+
+ }
 
 
 // Display country informations from city name (with populate)
