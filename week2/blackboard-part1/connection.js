350588a58121026604118d6c39339428c35211d2
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-03 10:26:42 +0200

    e

--- week2/blackboard-part1/connection.js
+++ week2/blackboard-part1/connection.js
@@ -1,5 +1,5 @@
 // Insert your connection string inside this variable
-const connectionString = 'mongodb+srv://Veldoran:Aju6QhdXyRsGtPv@cluster0.wofhepd.mongodb.net/blackboard';
+const connectionString = ' +srv://Veldoran:Aju6QhdXyRsGtPv@cluster0.wofhepd.mongodb.net/blackboard';
 
 module.exports = connectionString; // Do not edit/remove this line
     
