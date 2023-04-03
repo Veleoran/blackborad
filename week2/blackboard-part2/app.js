6e4ac411749beb666c713552d1b28d1c5141ac34
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-03 19:58:10 +0200

    e

--- week2/blackboard-part2/app.js
+++ week2/blackboard-part2/app.js
@@ -1,7 +1,12 @@
+require('./models/connection');
 const Article = require('./models/articles');
 const User = require('./models/users');
 const Order = require('./models/orders');
 
+
+/*
+** Articles
+*/
 
 function displayAllArticles() {
 	Article.find().then(data => {
@@ -44,51 +49,51 @@ function resetStocks() {
 ** Users
 */
 
-function displayAllUsers() { 
+function displayAllUsers() {
 	User.find().then(data => {
-		console.log(data);
-	   });
+		console.log('USERS =>', data);
+	});
 }
-//c est ok au dessus
 
 function deleteUser(userId) {
-	User.deleteOne({ _id: '6426e1660a5435fff043f99e' })
-	  .then(() => {
-		User.find().then((data) => {
-		  console.log(data);
-		});
-	  })
-	  .catch((error) => {
-		console.error('Erreur lors de la suppression de l\'utilisateur:', error);
-	  });
-  }
-  
-  // Pour utiliser la fonction deleteUser avec un ID spécifique :
-//   const userId = '6426e1660a5435fff043f99e';
-//   deleteUser(userId);
-  
+	User.deleteOne({ _id: userId }).then(() => {
+		console.log(`User ${userId} deleted`);
+	});
+}
+
+
 /*
 ** Orders
 */
-function displayAllOrders() {
-	Order.find().then((data) => {
-	  console.log('Orders =>', data);
-	});
-  }
-  
-  function updateOrderPaymentStatus(orderId, isPaid) {
-	// Ajoutez ici la logique pour mettre à jour le statut de paiement de la commande
-  }
-  function deleteOrder(orderId) {
-	Order.deleteOne({ _id: orderId })
-	  .then(() => {
-		console.log('Order deleted successfully');
-	  })
-	  .catch((error) => {
-		console.error('Erreur lors de la suppression de la commande:', error);
-	  });
 
-	}
+function displayAllOrders() {
+	Order.find().then(data => {
+		console.log('ORDERS =>', data);
+	});
+}
+
+function updateOrderPaymentStatus(orderId, isPaid) {
+	Order.updateOne({ _id: orderId }, { isPaid }).then(() => {
+		console.log(`Order ${orderId} updated`);
+	});
+}
+
+function deleteOrder(orderId) {
+	Order.deleteOne({ _id: orderId }).then(() => {
+		console.log(`Order ${orderId} deleted`);
+	});
+}
+
+
+/*
+** Query population
+*/
+
+function displayOrderArticles(orderId) { }
+
+function displayUserOrders(userId) { }
+
+
 // Do not edit/remove code under this line
 module.exports = {
 	displayAllArticles,
@@ -102,4 +107,6 @@ module.exports = {
 	displayAllOrders,
 	updateOrderPaymentStatus,
 	deleteOrder,
+	displayOrderArticles,
+	displayUserOrders,
 };
