18a7dc6f66b53ed91cb7233763c8d5a5464e9569
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-04 14:59:04 +0200

    orientexpressunpeulong

--- week3/orientexpress/routes/index.js
+++ week3/orientexpress/routes/index.js
@@ -3,6 +3,16 @@ var router = express.Router();
 
 let trips = [{ departure: 'Paris', arrival: 'Lyon' }, { departure: 'Lyon', arrival: 'Marseille' }];
 
+
+router.post('/trips', (req, res) => {
+trips.push ({departure : req.body.newdeparture, arrival : req.body.newarrival});
+res.json({ allTrips: trips });
+});
+
+
+
+
+
 // POST /trips - Sample result: 
 // "allTrips": [
 //   {
@@ -18,6 +28,15 @@ let trips = [{ departure: 'Paris', arrival: 'Lyon' }, { departure: 'Lyon', arriv
 //     "arrival": "Strasbourg"
 //   }
 // ]
+
+
+router.get('/trips', (req, res) => {
+    res.json({ allTrips: trips});
+
+});
+
+
+
 
 
 // GET /trips - Sample result:
@@ -36,11 +55,28 @@ let trips = [{ departure: 'Paris', arrival: 'Lyon' }, { departure: 'Lyon', arriv
 //   }
 // ]
 
+router.get('/lastTrip', (req, res) => {
+    
+    let lastTrip = trips[trips.length - 1];
+    res.json({lastTrip});
+})
+
+
+
+
+
 // GET /lastTrip - Sample result:
 // "lastTrip": {
 //   "departure": "Grenoble",
 //   "arrival": "Strasbourg"
 // }
+
+router.delete('/allTrips', (req, res) => {
+    trips = [];
+    res.json([]);
+});
+
+
 
 // DELETE /trips - Sample result:
 // "allTrips": []
