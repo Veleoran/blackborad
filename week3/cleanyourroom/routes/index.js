1a263e39ca26fedfba96aedcda02e909ef9d9267
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-06 15:33:53 +0200

    e

--- week3/cleanyourroom/routes/index.js
+++ week3/cleanyourroom/routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
 router.get('/home', (req, res) => {
   res.json({ message: 'Welcome to /home' });
@@ -10,52 +10,52 @@ router.get('/about', (req, res) => {
 });
 
 
-const { checkModeratorPassword} = require('../modules/checkPasswords');
+// const { checkModeratorPassword} = require('../modules/checkPasswords');
 
-router.post('/moderator/login', (req, res) => {
-  checkModeratorPassword('POST /login');
-  const password = 'modpwd123';
-  if (req.body.password === password) {
-    res.json({ result: true });
-  }
-  else {
-    res.json({ result: false, error: 'Invalid password for moderator' });
-  }
-});
+// router.post('/moderator/login', (req, res) => {
+//   checkModeratorPassword('POST /login');
+//   const password = 'modpwd123';
+//   if (req.body.password === password) {
+//     res.json({ result: true });
+//   }
+//   else {
+//     res.json({ result: false, error: 'Invalid password for moderator' });
+//   }
+// });
 
-router.post('/moderator/secretaction', (req, res) => {
-  checkModeratorPassword('POST/secretaction')
-  const password = 'modpwd123';
-  if (req.body.password === password) {
-    res.json({ result: true });
-  }
-  else {
-    res.json({ result: false, error: 'Invalid password for moderator' });
-  }
-});
+// router.post('/moderator/secretaction', (req, res) => {
+//   checkModeratorPassword('POST/secretaction')
+//   const password = 'modpwd123';
+//   if (req.body.password === password) {
+//     res.json({ result: true });
+//   }
+//   else {
+//     res.json({ result: false, error: 'Invalid password for moderator' });
+//   }
+// });
 
-const {checkAdminPassword} = require('../modules/checkPasswords')
+// const {checkAdminPassword} = require('../modules/checkPasswords')
 
-router.post('/admin/login', (req, res) => {
-  checkAdminPassword('POST/login')
-  const password = 'adminazerty123';
-  if (req.body.password === password) {
-    res.json({ result: true });
-  }
-  else {
-    res.json({ result: false, error: 'Invalid password for admin' });
-  }
-});
+// router.post('/admin/login', (req, res) => {
+//   checkAdminPassword('POST/login')
+//   const password = 'adminazerty123';
+//   if (req.body.password === password) {
+//     res.json({ result: true });
+//   }
+//   else {
+//     res.json({ result: false, error: 'Invalid password for admin' });
+//   }
+// });
 
-router.post('/admin/supersecretaction', (req, res) => {
-  checkAdminPassword('POST/supersecretaction')
-  const password = 'adminazerty123';
-  if (req.body.password === password) {
-    res.json({ result: true });
-  }
-  else {
-    res.json({ result: false, error: 'Invalid password for admin' });
-  }
-});
+// router.post('/admin/supersecretaction', (req, res) => {
+//   checkAdminPassword('POST/supersecretaction')
+//   const password = 'adminazerty123';
+//   if (req.body.password === password) {
+//     res.json({ result: true });
+//   }
+//   else {
+//     res.json({ result: false, error: 'Invalid password for admin' });
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
