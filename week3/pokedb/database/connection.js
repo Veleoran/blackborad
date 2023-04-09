451e1228d92321d3a4da2c34505aa25757af737a
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-09 22:17:57 +0200

    Mise à jour de la ressource pokedb

---
+++ week3/pokedb/database/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+// Insert your connection string inside this variable
+const connectionString = 'mongodb+srv://.../pokeDB';
+
+mongoose.set("strictQuery", true); // Remove Mongoose warning in console
+
+mongoose
+  .connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log("Database connected"))
+  .catch(error => console.error(error));
+
+
+module.exports = connectionString; // Do not edit/remove this line
