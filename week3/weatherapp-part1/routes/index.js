f3ae7dafba4f8384c94339a4b572aeb5f760f887
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-16 17:10:13 +0200

    e

--- week3/weatherapp-part1/routes/index.js
+++ week3/weatherapp-part1/routes/index.js
@@ -7,88 +7,43 @@ let weather = [
   { cityName: 'Stockholm', description: 'sunny', tempMin: 6.03, tempMax: 10.59 },
 ];
 
-// POST /weather - Sample result: 
-/*{
-  "result": true,
-  "weather": {
-    "cityName": "Lille",
-    "description": "sunny",
-    "tempMin": "15.2",
-    "tempMax": "22.7"
+router.post('/weather', (req, res) => {
+  const cityName = req.body.cityName.toLowerCase();
+  const description = req.body.description;
+  const tempMin = req.body.tempMin;
+  const tempMax = req.body.tempMax;
+
+  const cityExists = weather.find(city => city.cityName.toLowerCase() === cityName);
+
+  if (cityExists) {
+    res.json({ result: false, error: 'City already saved' });
+  } else {
+    const newCity = { cityName, description, tempMin, tempMax };
+    weather.push(newCity);
+    res.json({ result: true, weather: newCity });
   }
-} 
-OR
-{
-  "result": false,
-  "error": "City already saved"
-}
-*/
+});
+router.get('/weather', (req, res) => {
+  res.json({ weather: weather });
+});
+router.get('/weather/:cityName', (req, res) => {
+  const cityName = req.params.cityName.toLowerCase();
+  const city = weather.find(city => city.cityName.toLowerCase() === cityName);
 
-
-// GET /weather - Sample result: 
-/*{
-  "weather": [
-    {
-      "city": "London",
-      "description": "cloudy",
-      "tempMin": 12.95,
-      "tempMax": 17.39
-    },
-    {
-      "city": "Rio de Janeiro",
-      "description": "sunny",
-      "tempMin": 23.98,
-      "tempMax": 28.63
-    },
-    {
-      "city": "Stockholm",
-      "description": "sunny",
-      "tempMin": 6.03,
-      "tempMax": 10.59
-    }
-  ]
-} */
-
-
-// GET /weather/:cityName - Sample result: 
-/*{
-  "result": true,
-  "weather": {
-    "cityName": "London",
-    "description": "cloudy",
-    "tempMin": 12.95,
-    "tempMax": 17.39
+  if (city) {
+    res.json({ result: true, weather: city });
+  } else {
+    res.json({ result: false, error: 'City not found' });
   }
-}
-OR
-{
-  "result": false,
-  "error": "City not found"
-} */
+});
+router.delete('/weather/:cityName', (req, res) => {
+  const cityName = req.params.cityName.toLowerCase();
+  const cityIndex = weather.findIndex(city => city.cityName.toLowerCase() === cityName);
 
-
-// DELETE /weather/:cityName - Sample result: 
-/*{
-  "result": true,
-  "weather": [
-    {
-      "cityName": "Rio de Janeiro",
-      "description": "sunny",
-      "tempMin": 23.98,
-      "tempMax": 28.63
-    },
-    {
-      "cityName": "Stockholm",
-      "description": "sunny",
-      "tempMin": 6.03,
-      "tempMax": 10.59
-    }
-  ]
-}
-OR
-{
-  "result": false,
-  "error": "City not found"
-} */
-
-module.exports = router;
+  if (cityIndex !== -1) {
+    weather.splice(cityIndex, 1);
+    res.json({ result: true, weather: weather });
+  } else {
+    res.json({ result: false, error: 'City not found' });
+  }
+});
