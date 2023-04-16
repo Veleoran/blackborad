406eb3f8bb8f12ecb8a1359de10b392b566240de
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-16 17:21:46 +0200

    e

--- week3/weatherapp-part1/app.js
+++ week3/weatherapp-part1/app.js
@@ -9,7 +9,13 @@ var app = express();
 
 const cors = require('cors');
 app.use(cors());
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
