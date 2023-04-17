c61dfcf787a75fb7d9fc41fe46c7fac41a75011a
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-17 10:01:44 +0200

    mymov3

--- week5/mymoviz-part3/components/Movie.js
+++ week5/mymoviz-part3/components/Movie.js
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/Movie.module.css';
+import { useState } from 'react';
 
 function Movie(props) {
   const stars = [];
@@ -11,17 +12,77 @@ function Movie(props) {
     }
     stars.push(<FontAwesomeIcon key={i} icon={faStar} style={style} />);
   }
+ 
+    const [personalNote, setPersonalNote] = useState(0);
+    const [watchCount, setWatchCount] = useState(0);
+    const [like, setLike] = useState(false);}
 
+const rateMovie = (rating) => {
+  setPersonalNote(rating);
+
+
+const incrementWatchCount = () => {
+  setWatchCount(watchCount + 1);
+};
+
+const toggleLike = () => {
+  setLike(!like);
+};
+const renderPersonalRatingStars = () => {
+  const stars = [];
+  for (let i = 1; i <= 10; i++) {
+    const color = i <= personalNote ? '#2196f3' : 'black';
+    stars.push(
+      <FontAwesomeIcon
+        key={i}
+        className="note"
+        icon={faStar}
+        style={{ color, cursor: 'pointer' }}
+        onClick={() => rateMovie(i)}
+      />
+    );
+  }
+  return stars;
+};
+
+const renderWatchIcon = () => {
   return (
-    <div className={styles.card}>
-      <img className={styles.image} src={props.poster} alt={props.title} />
-      <div className={styles.textContainer}>
-        <span className={styles.name}>{props.title}</span>
-        <p className={styles.description}>{props.overview}</p>
-        <span className={styles.vote}>{stars} ({props.voteCount})</span>
+    <FontAwesomeIcon
+      className="watch"
+      icon={faVideo}
+      style={{ color: watchCount > 0 ? '#e74c3c' : 'black', cursor: 'pointer' }}
+      onClick={incrementWatchCount}
+    />
+  );
+};
+
+const renderLikeIcon = () => {
+  return (
+    <FontAwesomeIcon
+      className="like"
+      icon={faHeart}
+      style={{ color: like ? '#e74c3c' : 'black', cursor: 'pointer' }}
+      onClick={toggleLike}
+    />
+  );
+};
+
+  
+return (
+  <div className={styles.card}>
+    <img className={styles.image} src={props.poster} alt={props.title} />
+    <div className={styles.textContainer}>
+      <span className={styles.name}>{props.title}</span>
+      <p className={styles.description}>{props.overview}</p>
+      <span className={styles.vote}>{stars} ({props.voteCount})</span>
+      <div className={styles.interactions}>
+        <div>{renderPersonalRatingStars()} ({personalNote})</div>
+        <div>{renderWatchIcon()} ({watchCount})</div>
+        <div>{renderLikeIcon()}</div>
       </div>
     </div>
-  );
-}
+  </div>
+);
+};
 
 export default Movie;
