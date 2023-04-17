7c88e649984ec4824b1952011f9092687d38ffd7
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-17 09:16:24 +0200

    e

--- week5/countwithme/components/Home.js
+++ week5/countwithme/components/Home.js
@@ -1,42 +1,43 @@
+import { useState } from 'react';
 import styles from '../styles/Home.module.css';
-import React, { useState } from "react";
 
 function Home() {
-  const [count, setCount] = useState(0);
+  // 1. Import useState and 2. Initialize counter state
+  const [counter, setCounter] = useState(0);
 
+  // 3. Functions to increment and decrement the counter
   const incrementCount = () => {
-    setCount(count + 1);
+    setCounter(counter + 1);
   };
 
   const decrementCount = () => {
-    if (count > 0) {
-      setCount(count - 1);
+    if (counter > 0) {
+      setCounter(counter - 1);
     }
   };
 
   return (
-    <div>
-      <h1>Compteur : {count}</h1>
-      <button id="increment-btn" onClick={incrementCount}>
-        +1
-      </button>
-      <button id="decrement-btn" onClick={decrementCount}>
-        -1
-      </button>
-    </div>
-  );
-}
-
-
-
-function Home() {
-  return (
     <div className={styles.container}>
       <img className={styles.logo} src="logo.png" />
       <div className={styles.counterContainer}>
-        <button className={styles.decrementBtn} id="decrementBtn">-</button>
-        <span className={styles.counter} id="counter">{counter}</span>
-        <button className={styles.incrementBtn} id="incrementBtn">+</button>
+        {/* 4. Add onClick event handlers */}
+        <button
+          className={styles.decrementBtn}
+          id="decrementBtn"
+          onClick={decrementCount}
+        >
+          -
+        </button>
+        <span className={styles.counter} id="counter">
+          {counter}
+        </span>
+        <button
+          className={styles.incrementBtn}
+          id="incrementBtn"
+          onClick={incrementCount}
+        >
+          +
+        </button>
       </div>
     </div>
   );
