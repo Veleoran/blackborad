2a9b04226f6a90d3c9b4661d43c468da8e5e2977
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-17 09:07:20 +0200

    e

--- week5/countwithme/components/Home.js
+++ week5/countwithme/components/Home.js
@@ -1,4 +1,33 @@
 import styles from '../styles/Home.module.css';
+import React, { useState } from "react";
+
+function Home() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Compteur : {count}</h1>
+      <button id="increment-btn" onClick={incrementCount}>
+        +1
+      </button>
+      <button id="decrement-btn" onClick={decrementCount}>
+        -1
+      </button>
+    </div>
+  );
+}
+
+export default Home;
 
 function Home() {
   return (
