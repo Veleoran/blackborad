1cbb277b7f53ec6299e7eb8f5a6feade4308955a
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-17 12:01:15 +0200

    yamsrien

--- week5/yams-part2/components/Dice.js
+++ week5/yams-part2/components/Dice.js
@@ -1,11 +1,23 @@
-import styles from '../styles/Dice.module.css';
+import React, { useState } from "react";
+import styles from "../styles/Dice.module.css";
 
-function Dice(props) {
+const Dice = ({ value }) => {
+  const [dieValue, setDieValue] = useState(value);
+
+  const handleClick = () => {
+    setDieValue(Math.floor(Math.random() * 6) + 1);
+  };
+
   return (
     <div>
-      <img className={styles.dice} src={`${props.number}.png`} alt={props.number} />
+      <img
+        className={styles.dice}
+        src={`path/to/dice-images/dice-${dieValue}.png`}
+        alt={dieValue}
+        onClick={handleClick}
+      />
     </div>
   );
-}
+};
 
 export default Dice;
--- week5/yams-part2/components/Home.js
+++ week5/yams-part2/components/Home.js
@@ -1,11 +1,17 @@
-import Dice from './Dice';
-import styles from '../styles/Home.module.css';
+import React, { useState } from "react";
+import Dice from "./Dice";
+import styles from "../styles/Home.module.css";
 
-function Home() {
-  const dices = [];
-  for (let i = 0; i < 5; i++) {
-    dices.push(<Dice key={i} number={i + 1} />);
-  }
+const Home = () => {
+  const [gameStarted, setGameStarted] = useState(false);
+  const [dices, setDices] = useState(
+    Array.from({ length: 5 }, (_, i) => i + 1)
+  );
+
+  const handleLaunch = () => {
+    setGameStarted(true);
+    setDices(dices.map(() => Math.floor(Math.random() * 6) + 1));
+  };
 
   return (
     <div className={styles.container}>
@@ -17,16 +23,23 @@ function Home() {
       <div className={styles.middleSection}>
         <img src="logo.png" alt="Logo" className={styles.logo} />
         <div className={styles.board}>
-          {dices}
+          {dices.map((value, index) => (
+            <Dice key={index} value={value} />
+          ))}
         </div>
       </div>
       <div>
         <div className={styles.triangleRight}></div>
         <div className={styles.triangleRightRed}></div>
         <div className={styles.triangleRight}></div>
+        {!gameStarted && (
+          <button className="launchBtn" onClick={handleLaunch}>
+            Launch
+          </button>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default Home;
