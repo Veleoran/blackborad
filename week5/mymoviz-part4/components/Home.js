4660621c7e07e0a3448f445278a84958b33036df
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-18 14:24:37 +0200

    Mise à jour de la ressource mymoviz-part4

--- week5/mymoviz-part4/components/Home.js
+++ week5/mymoviz-part4/components/Home.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import Movie from './Movie';
 import 'antd/dist/antd.css';
 import styles from '../styles/Home.module.css';
@@ -6,40 +5,23 @@ import { Popover, Button } from 'antd';
 
 function Home() {
   const moviesData = [
-    // ...
+    { title: 'Forrest Gump', poster: 'forrestgump.jpg', voteAverage: 9.2, voteCount: 22_705, overview: 'A man with a low IQ has accomplished great things in his life and been present during significant historic events—in each case.' },
+    { title: 'The Dark Knight', poster: 'thedarkknight.jpg', voteAverage: 8.5, voteCount: 27_547, overview: 'Batman raises the stakes in his war on crime and sets out to dismantle the remaining criminal organizations that plague the streets.' },
+    { title: 'Your name', poster: 'yourname.jpg', voteAverage: 8.5, voteCount: 8_691, overview: 'High schoolers Mitsuha and Taki are complete strangers living separate lives. But one night, they suddenly switch places.' },
+    { title: 'Iron Man', poster: 'ironman.jpg', voteAverage: 7.6, voteCount: 22_7726, overview: 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.' },
+    { title: 'Inception', poster: 'inception.jpg', voteAverage: 8.4, voteCount: 31_546, overview: 'Cobb, a skilled thief who commits corporate espionage by infiltrating the subconscious of his targets is offered a chance to regain his old life.' },
   ];
 
-  const [likedMovies, setLikedMovies] = useState([]);
-
-  const updateLikedMovies = (movieTitle, action) => {
-    if (action === 'add') {
-      setLikedMovies([...likedMovies, movieTitle]);
-    } else if (action === 'remove') {
-      setLikedMovies(likedMovies.filter((title) => title !== movieTitle));
-    }
-  };
-
   const movies = moviesData.map((data, i) => {
-    return (
-      <Movie
-        key={i}
-        title={data.title}
-        overview={data.overview}
-        poster={data.poster}
-        voteAverage={data.voteAverage}
-        voteCount={data.voteCount}
-        onLike={updateLikedMovies}
-      />
-    );
+    return <Movie key={i} title={data.title} overview={data.overview} poster={data.poster} voteAverage={data.voteAverage} voteCount={data.voteCount} />;
   });
 
   const popoverContent = (
     <div className={styles.popoverContent}>
-      {likedMovies.map((movie, index) => (
-        <span key={index}>
-          {movie} <button onClick={() => updateLikedMovies(movie, 'remove')}>x</button>
-        </span>
-      ))}
+      <span>Movie 1</span>
+      <span>Movie 2</span>
+      <span>Movie 3</span>
+      <span>Movie 4</span>
     </div>
   );
 
@@ -51,7 +33,7 @@ function Home() {
           <img className={styles.logo} src="logoletter.png" alt="Letter logo" />
         </div>
         <Popover title="Liked movies" content={popoverContent} className={styles.popover} trigger="click">
-          <Button>♥ {likedMovies.length} movie(s)</Button>
+          <Button>♥ 4 movie(s)</Button>
         </Popover>
       </div>
       <div className={styles.title}>LAST RELEASES</div>
