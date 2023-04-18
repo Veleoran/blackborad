98220cfcaf9b0235fdbadc493269cd7c89e0101e
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-18 12:00:12 +0200

    Mise à jour de la ressource mymoviz-part4

--- week5/mymoviz-part4/components/Movie.js
+++ week5/mymoviz-part4/components/Movie.js
@@ -1,51 +1,65 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faStar, faVideo } from '@fortawesome/free-solid-svg-icons';
-
 import styles from '../styles/Movie.module.css';
 
 function Movie(props) {
-  // State to toggle movie description
-  const [showDescription, setShowDescription] = useState(false);
+  const [watchCount, setWatchCount] = useState(0);
+  const [like, setLike] = useState(false);
+  const [personalNote, setPersonalNote] = useState(0);
+
+  // Average evaluation
+  const stars = [];
+  for (let i = 0; i < 10; i++) {
+    let style = {};
+    if (i < props.voteAverage - 1) {
+      style = { 'color': '#f1c40f' };
+    }
+    stars.push(<FontAwesomeIcon key={i} icon={faStar} style={style} />);
+  }
+
+  // Watch movie
+  const handleWatchMovie = () => {
+    setWatchCount(watchCount + 1);
+  };
+  let videoIconStyle = { 'cursor': 'pointer' };
+  if (watchCount > 0) {
+    videoIconStyle = { 'color': '#e74c3c', 'cursor': 'pointer' };
+  }
 
   // Like movie
   const handleLikeMovie = () => {
-    props.onHeartClick(props.title);
+    setLike(!like);
   };
+  let heartIconStyle = { 'cursor': 'pointer' };
+  if (like) {
+    heartIconStyle = { 'color': '#e74c3c', 'cursor': 'pointer' };
+  }
 
-  // Toggle movie description
-  const toggleDescription = () => {
-    setShowDescription(!showDescription);
-  };
-
-  let heartIconStyle = props.isLiked
-    ? { color: '#e74c3c', cursor: 'pointer' }
-    : { cursor: 'pointer' };
+  // Personal note
+  const personalStars = [];
+  for (let i = 0; i < 10; i++) {
+    let style = { 'cursor': 'pointer' };
+    if (i < personalNote) {
+      style = { 'color': '#2196f3', 'cursor': 'pointer' };
+    }
+    personalStars.push(<FontAwesomeIcon key={i} icon={faStar} onClick={() => setPersonalNote(i + 1)} style={style} className="note" />);
+  }
 
   return (
     <div className={styles.card}>
-      {/* ... */}
+      <img className={styles.image} src={props.poster} alt={props.title} />
       <div className={styles.textContainer}>
-        {/* ... */}
-        <div className={styles.iconContainer}>
-          {/* ... */}
-          <span>
-            <FontAwesomeIcon
-              icon={faHeart}
-              onClick={() => handleLikeMovie()}
-              style={heartIconStyle}
-              className="like"
-            />
-          </span>
-          {/* Add star and video icons */}
-          <span>
-            <FontAwesomeIcon icon={faStar} className={styles.starIcon} />
-          </span>
-          <span>
-            <FontAwesomeIcon icon={faVideo} className={styles.videoIcon} onClick={toggleDescription} />
-          </span>
+        <div>
+          <span className={styles.name}>{props.title}</span>
+          <p className={styles.description}>{props.overview}</p>
         </div>
-        {showDescription && <p className={styles.description}>{props.overview}</p>}
+        <div className={styles.iconContainer}>
+          <span className={styles.vote}>{stars} ({props.voteCount})</span>
+          <span>{personalStars} ({personalNote})</span>
+          <span><FontAwesomeIcon icon={faVideo} onClick={() => handleWatchMovie()} style={videoIconStyle} className="watch" /> ({watchCount})</span>
+          <span><FontAwesomeIcon icon={faHeart} onClick={() => handleLikeMovie()} style={heartIconStyle} className="like" /></span>
+        </div>
       </div>
     </div>
   );
