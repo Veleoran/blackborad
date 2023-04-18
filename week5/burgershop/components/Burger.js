474f9021b67a8bd9534ee2cbe882dc829f2d2b83
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-18 09:23:21 +0200

    royalcheese motherfcker

--- week5/burgershop/components/Burger.js
+++ week5/burgershop/components/Burger.js
@@ -1,26 +1,16 @@
 import styles from '../styles/Burger.module.css';
 
-function Burger(props) {
-	return (
-		<div className={styles.container}>
-			<h2 className={styles.name}>{props.name}</h2>
-			<img className={styles.images} src={props.image} />
-			<div className={styles.btnContainer}>
-
-			</div>
-		</div>
-	);
+function Burger({ name, image, onSelect, onRemove }) {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.name}>{name}</h2>
+      <img className={styles.images} src={image} />
+      <div className={styles.btnContainer}>
+        <button className="selectBtn" onClick={() => onSelect(name)}>Select</button>
+        <button className="removeBtn" onClick={() => onRemove(name)}>Remove all</button>
+      </div>
+    </div>
+  );
 }
 
-function Burger({ onSelect, onRemove }) {
-	const burgerName = "The Californian";
-  
-	return (
-	  <div>
-		<button className="selectBtn" onClick={() => onSelect(burgerName)}>Select</button>
-		<button className="removeBtn" onClick={() => onRemove(burgerName)}>Remove all</button>
-	  </div>
-	);
-  }
-  
 export default Burger;
--- week5/burgershop/components/Home.js
+++ week5/burgershop/components/Home.js
@@ -3,40 +3,43 @@ import Burger from './Burger';
 import styles from '../styles/Home.module.css';
 
 function Home() {
-
   const burgersData = [
     { name: 'The New Yorker', image: 'newyorker.jpg' },
     { name: 'The Californian', image: 'californian.jpg' },
     { name: 'The Parisian', image: 'parisian.jpg' },
   ];
 
+  const [selectedBurgers, setSelectedBurgers] = useState([]);
+
   const burgers = burgersData.map((data, i) => {
-    return <Burger key={i} name={data.name} image={data.image} />;
+    return (
+      <Burger
+        key={i}
+        name={data.name}
+        image={data.image}
+        onSelect={selectBurger}
+        onRemove={removeBurger}
+      />
+    );
   });
 
   function selectBurger(newBurger) {
     setSelectedBurgers([...selectedBurgers, newBurger]);
   }
+  
   function removeBurger(burgerToDelete) {
-  setSelectedBurgers(selectedBurgers.filter(burger => burger !== burgerToDelete));
-}
-
+    setSelectedBurgers(selectedBurgers.filter(burger => burger !== burgerToDelete));
+  }
 
   return (
     <div className={styles.container}>
       <img src="logo.png" className={styles.logo} />
-      <h4 className={styles.text}>Number of burgers selected: <span id="counter"></span></h4>
-      <div className={styles.burgerContainer}>
-        {burgers}
-        <div>
-    <Burger onSelect={selectBurger} onRemove={removeBurger} />
-    <p>Number of burgers selected: {selectedBurgers.length}</p>
-  </div>
-      </div>
+      <h4 className={styles.text}>
+        Number of burgers selected: <span id="counter">{selectedBurgers.length}</span>
+      </h4>
+      <div className={styles.burgerContainer}>{burgers}</div>
     </div>
   );
 }
-
-
 
 export default Home;
