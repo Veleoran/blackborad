725a5970d3207363e9f766a403d3385489c98b20
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-18 10:53:11 +0200

    e

--- week5/mymoviz-part4/components/Movie.js
+++ week5/mymoviz-part4/components/Movie.js
@@ -4,61 +4,39 @@ import { faHeart, faStar, faVideo } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/Movie.module.css';
 
 function Movie(props) {
-  const [watchCount, setWatchCount] = useState(0);
-  const [like, setLike] = useState(false);
-  const [personalNote, setPersonalNote] = useState(0);
-
-  // Average evaluation
-  const stars = [];
-  for (let i = 0; i < 10; i++) {
-    let style = {};
-    if (i < props.voteAverage - 1) {
-      style = { 'color': '#f1c40f' };
-    }
-    stars.push(<FontAwesomeIcon key={i} icon={faStar} style={style} />);
-  }
-
-  // Watch movie
-  const handleWatchMovie = () => {
-    setWatchCount(watchCount + 1);
-  };
-  let videoIconStyle = { 'cursor': 'pointer' };
-  if (watchCount > 0) {
-    videoIconStyle = { 'color': '#e74c3c', 'cursor': 'pointer' };
-  }
+  // ...
 
   // Like movie
   const handleLikeMovie = () => {
-    setLike(!like);
+    props.onHeartClick(props.title);
   };
-  let heartIconStyle = { 'cursor': 'pointer' };
-  if (like) {
-    heartIconStyle = { 'color': '#e74c3c', 'cursor': 'pointer' };
-  }
 
-  // Personal note
-  const personalStars = [];
-  for (let i = 0; i < 10; i++) {
-    let style = { 'cursor': 'pointer' };
-    if (i < personalNote) {
-      style = { 'color': '#2196f3', 'cursor': 'pointer' };
+  let heartIconStyle = props.isLiked
+    ? { color: '#e74c3c', cursor: 'pointer' }
+    : { cursor: 'pointer' };
+
+    function Movie({ onUpdateLikes }) {
     }
-    personalStars.push(<FontAwesomeIcon key={i} icon={faStar} onClick={() => setPersonalNote(i + 1)} style={style} className="note" />);
-  }
 
   return (
     <div className={styles.card}>
-      <img className={styles.image} src={props.poster} alt={props.title} />
+      {/* ... */}
       <div className={styles.textContainer}>
-        <div>
-          <span className={styles.name}>{props.title}</span>
-          <p className={styles.description}>{props.overview}</p>
-        </div>
+        {/* ... */}
         <div className={styles.iconContainer}>
-          <span className={styles.vote}>{stars} ({props.voteCount})</span>
-          <span>{personalStars} ({personalNote})</span>
-          <span><FontAwesomeIcon icon={faVideo} onClick={() => handleWatchMovie()} style={videoIconStyle} className="watch" /> ({watchCount})</span>
-          <span><FontAwesomeIcon icon={faHeart} onClick={() => handleLikeMovie()} style={heartIconStyle} className="like" /></span>
+          {/* ... */}
+          <div className="heart" onClick={() => props.onHeartClick(props.movie.title)}>
+          <span className="heart" onClick={() => onUpdateLikes(movieTitle)}>❤️</span>
+  <FontAwesomeIcon icon={faHeart} />
+</div>
+          <span>
+            <FontAwesomeIcon
+              icon={faHeart}
+              onClick={() => handleLikeMovie()}
+              style={heartIconStyle}
+              className="like"
+            />
+          </span>
         </div>
       </div>
     </div>
