d31527fdf80d602914b110120de2f7b731fd67c6
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-19 08:14:50 +0200

    555

--- week5/mymoviz-part5/frontend/components/Home.js
+++ week5/mymoviz-part5/frontend/components/Home.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Popover, Button } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
@@ -8,8 +9,31 @@ import styles from '../styles/Home.module.css';
 
 function Home() {
   const [likedMovies, setLikedMovies] = useState([]);
+  const [moviesData, setMoviesData] = useState([]);
 
-  // Liked movies (inverse data flow)
+  const formatMovieData = (movie) => {
+    const baseUrl = 'https://image.tmdb.org/t/p/w500';
+    const maxLength = 250;
+
+    return {
+      ...movie,
+      poster: baseUrl + movie.poster_path,
+      overview: movie.overview.length > maxLength
+        ? movie.overview.substring(0, maxLength) + '...'
+        : movie.overview
+    };
+  };
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await axios.get('/movies');
+      const formattedMovies = response.data.movies.map(formatMovieData);
+      setMoviesData(formattedMovies);
+    };
+
+    fetchMovies();
+  }, []);
+
   const updateLikedMovies = (movieTitle) => {
     if (likedMovies.find(movie => movie === movieTitle)) {
       setLikedMovies(likedMovies.filter(movie => movie !== movieTitle));
@@ -18,14 +42,12 @@ function Home() {
     }
   };
 
-  const likedMoviesPopover = likedMovies.map((data, i) => {
-    return (
-      <div key={i} className={styles.likedMoviesContainer}>
-        <span className="likedMovie">{data}</span>
-        <FontAwesomeIcon icon={faCircleXmark} onClick={() => updateLikedMovies(data)} className={styles.crossIcon} />
-      </div>
-    );
-  });
+  const likedMoviesPopover = likedMovies.map((data, i) => (
+    <div key={i} className={styles.likedMoviesContainer}>
+      <span className="likedMovie">{data}</span>
+      <FontAwesomeIcon icon={faCircleXmark} onClick={() => updateLikedMovies(data)} className={styles.crossIcon} />
+    </div>
+  ));
 
   const popoverContent = (
     <div className={styles.popoverContent}>
@@ -33,18 +55,9 @@ function Home() {
     </div>
   );
 
-  // Movies list
-  const moviesData = [
-    { title: 'Forrest Gump', poster: 'forrestgump.jpg', voteAverage: 9.2, voteCount: 22_705, overview: 'A man with a low IQ has accomplished great things in his life and been present during significant historic events—in each case.' },
-    { title: 'The Dark Knight', poster: 'thedarkknight.jpg', voteAverage: 8.5, voteCount: 27_547, overview: 'Batman raises the stakes in his war on crime and sets out to dismantle the remaining criminal organizations that plague the streets.' },
-    { title: 'Your name', poster: 'yourname.jpg', voteAverage: 8.5, voteCount: 8_691, overview: 'High schoolers Mitsuha and Taki are complete strangers living separate lives. But one night, they suddenly switch places.' },
-    { title: 'Iron Man', poster: 'ironman.jpg', voteAverage: 7.6, voteCount: 22_7726, overview: 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.' },
-    { title: 'Inception', poster: 'inception.jpg', voteAverage: 8.4, voteCount: 31_546, overview: 'Cobb, a skilled thief who commits corporate espionage by infiltrating the subconscious of his targets is offered a chance to regain his old life.' },
-  ];
-
   const movies = moviesData.map((data, i) => {
     const isLiked = likedMovies.some(movie => movie === data.title);
-    return <Movie key={i} updateLikedMovies={updateLikedMovies} isLiked={isLiked} title={data.title} overview={data.overview} poster={data.poster} voteAverage={data.voteAverage} voteCount={data.voteCount} />;
+    return <Movie key={i} updateLikedMovies={updateLikedMovies} isLiked={isLiked} title={data.title} overview={data.overview} poster={data.poster} voteAverage={data.vote_average} voteCount={data.vote_count} />;
   });
 
   return (
