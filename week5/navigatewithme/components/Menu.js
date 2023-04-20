d70ed455a639346857222166f6e131e85b837148
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-20 10:38:32 +0200

    e

--- week5/navigatewithme/components/Menu.js
+++ week5/navigatewithme/components/Menu.js
@@ -1,13 +1,20 @@
 import styles from '../styles/Menu.module.css';
+import Link from 'next/link';
 
 function Menu() {
   return (
     <div className={styles.header}>
       <span className={styles.logo}>Navigate with me</span>
       <div className={styles.linkContainer}>
-        <span className={styles.link}>Home</span>
-        <span className={styles.link}>Shop</span>
-        <span className={styles.link}>About</span>
+        <Link href="/">
+          <a className={styles.link}>Home</a>
+        </Link>
+        <Link href="/shop">
+          <a className={styles.link}>Shop</a>
+        </Link>
+        <Link href="/about">
+          <a className={styles.link}>About</a>
+        </Link>
       </div>
     </div>
   );
