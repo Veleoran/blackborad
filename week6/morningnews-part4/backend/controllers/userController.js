5b506a52fdd600f00a48d97763e6d1bf7580ee76
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-25 11:51:41 +0200

    e

--- week6/morningnews-part4/backend/controllers/userController.js
+++ week6/morningnews-part4/backend/controllers/userController.js
@@ -11,3 +11,27 @@ const hashPassword = async (password) => {
   const hashedPassword = await bcrypt.hash(password, salt);
   return hashedPassword;
 };
+
+
+exports.canBookmark = async (req, res) => {
+    try {
+      const token = req.params.token;
+  
+      if (!token) {
+        res.status(400).json({ result: false, error: 'Missing token' });
+        return;
+      }
+  
+      const user = await User.findOne({ token });
+  
+      if (!user) {
+        res.status(400).json({ result: false, error: 'User not found' });
+        return;
+      }
+  
+      res.json({ result: true, canBookmark: user.canBookmark });
+    } catch (error) {
+      res.status(500).json({ result: false, error });
+    }
+  };
+  
--- week6/morningnews-part4/backend/routes/users.js
+++ week6/morningnews-part4/backend/routes/users.js
@@ -7,43 +7,56 @@ require('../models/connection');
 const User = require('../models/users');
 const { checkBody } = require('../modules/checkBody');
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
 	if (!checkBody(req.body, ['username', 'password'])) {
     res.json({ result: false, error: 'Missing or empty fields' });
     return;
   }
 
   // Check if the user has not already been registered
-  User.findOne({ username: req.body.username }).then(data => {
-    if (data === null) {
-      const newUser = new User({
-        username: req.body.username,
-        password: req.body.password,
-      });
+  const data = await User.findOne({ username: req.body.username });
+  if (data === null) {
+    const token = uid2(32);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-      newUser.save().then(() => {
-        res.json({ result: true });
-      });
-    } else {
-      // User already exists in database
-      res.json({ result: false, error: 'User already exists' });
-    }
-  });
+    const newUser = new User({
+      username: req.body.username,
+      password: hashedPassword,
+      token: token,
+    });
+
+    newUser.save().then(() => {
+      res.json({ result: true, token: token });
+    });
+  } else {
+    // User already exists in database
+    res.json({ result: false, error: 'User already exists' });
+  }
 });
 
-router.post('/signin', (req, res) => {
+router.post('/signin', async (req, res) => {
   if (!checkBody(req.body, ['username', 'password'])) {
     res.json({ result: false, error: 'Missing or empty fields' });
     return;
   }
+  
 
-  User.findOne({ username: req.body.username, password: req.body.password }).then(data => {
-    if (data) {
-      res.json({ result: true });
+  const user = await User.findOne({ username: req.body.username });
+
+  if (user) {
+    const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+
+    if (passwordIsValid) {
+      res.json({ result: true, token: user.token });
     } else {
-      res.json({ result: false, error: 'User not found' });
+      res.json({ result: false, error: 'Invalid password' });
     }
-  });
+  } else {
+    res.json({ result: false, error: 'User not found' });
+  }
 });
+
+router.get('/users/canBookmark/:token', userController.canBookmark);
+router.get('/canBookmark/:token', userController.canBookmark);
 
 module.exports = router;
