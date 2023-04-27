9b60abcef371074d3b3e712091633bca316db0d4
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-27 11:14:36 +0200

    e

--- week3/weatherapp-part2/frontend/script.js
+++ week3/weatherapp-part2/frontend/script.js
@@ -1,6 +1,4 @@
-// const  response  = require("../backend/app");
 
-// Insert your code here
 fetch('http://localhost:3000/weather')
 	.then(response => response.json())
 	.then(data => {
@@ -24,31 +22,31 @@ fetch('http://localhost:3000/weather')
 		}
 	});
 
-  function updateDeleteCityEventListener() {
-    for (let i = 0; i < document.querySelectorAll('.deleteCity').length; i++) {
-      document.querySelectorAll('.deleteCity')[i].addEventListener('click', function () {
-        fetch(`http://localhost:3000/weather/${this.id}`, { method: 'DELETE' })
-          .then(response => response.json())
-          .then(data => {
-            if (data.result) {
-              this.parentNode.remove();
-            }
-          });
-      });
-    }
-  }
-   
-document.querySelector("#addCity").addEventListener("click", function() {
-const cityName = document.querySelector("cityNameInput").value;
+function updateDeleteCityEventListener() {
+	for (let i = 0; i < document.querySelectorAll('.deleteCity').length; i++) {
+		document.querySelectorAll('.deleteCity')[i].addEventListener('click', function () {
+			fetch(`http://localhost:3000/weather/${this.id}`, { method: 'DELETE' })
+				.then(response => response.json())
+				.then(data => {
+					if (data.result) {
+						this.parentNode.remove();
+					}
+				});
+		});
+	}
+}
 
-fetch(`http://localhost:3000/weather`, {
-method: "POST",
-headers: {"Content-Type": "application/json" },
-body: JSON.stringify({cityName}),
-}).then(response => response.json())
-.then(data => {
-    if (data.result) {
-      document.querySelector('#cityList').innerHTML += `
+document.querySelector('#addCity').addEventListener('click', function () {
+	const cityName = document.querySelector('#cityNameInput').value;
+
+	fetch('http://localhost:3000/weather', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ cityName }),
+	}).then(response => response.json())
+		.then(data => {
+			if (data.result) {
+				document.querySelector('#cityList').innerHTML += `
 			<div class="cityContainer">
 				<p class="name">${data.weather.cityName}</p>
 				<p class="description">${data.weather.description}</p>
@@ -67,6 +65,3 @@ body: JSON.stringify({cityName}),
 
 		});
 });
-
-
-// return data
