906a1b923ddf39d3c76f2a8934770b4442334914
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-29 00:24:07 +0200

    erwan

--- week4/stockmanager/routes/recalls.js
+++ week4/stockmanager/routes/recalls.js
@@ -1,12 +1,21 @@
-const express = require('express');
-const router = express.Router();
+const products = require('../data');
 
-const productsController = require('../controllers/productsController');
+exports.getRecalledProductsByBrand = async (brand) => {
+  const recalledProducts = products.filter(
+    (product) =>
+      product.brand === brand &&
+      product.batches.some((batch) => batch.recall)
+  );
+  const recalledProductIds = recalledProducts.map((product) => product.id);
+  return { products: recalledProductIds };
+};
 
-
-
-router.get('/byId/:id', productsController.getById);
-router.get('/byBrand/:brand', productsController.getByBrand);
-router.get('/byBatchId/:id', productsController.getByBatchId);
-
-module.exports = router;
+exports.getRecalledProductsByTimestamp = async (timestamp) => {
+  const recalledProducts = products.filter((product) =>
+    product.batches.some(
+      (batch) => batch.recall && Date.parse(batch.expirationDate) < timestamp
+    )
+  );
+  const recalledProductIds = recalledProducts.map((product) => product.id);
+  return { products: recalledProductIds };
+};
