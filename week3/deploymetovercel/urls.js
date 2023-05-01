79e136e0e7239abfe92b0833d97e49e4faf1152b
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-05-01 21:27:14 +0200

    e

--- week3/deploymetovercel/urls.js
+++ week3/deploymetovercel/urls.js
@@ -1,7 +1,8 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = 'https://whatyearbk-u4am.vercel.app/';
+const BACKEND_URL = 'https://whatyearisit-backend.vercel.app/';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = 'https://whatyearisit-frontend.vercel.app/';
+
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
