7c0ef9e461eafd5cedb5647c10aac5b637eea4bf
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-05-04 23:23:26 +0200

    e

--- week6/evaluation-week6/backend/routes/users.js
+++ week6/evaluation-week6/backend/routes/users.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const uid2 = require("uid2");
+
+
 router.post('/signup', (req, res) => {
 	const newUserInfos = {
 		username: req.body.username,
-		token: 'This is not a token', // Change this line by using uid2
+		token: uid2(32), // Change this line by using uid2
 	};
 
 	res.json({ result: true, newUserInfos: newUserInfos });
