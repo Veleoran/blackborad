675e5fe27e5c122489e454fb24bacc8ff43854c9
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-05-04 11:31:08 +0200

    e

--- week7/locapic-part3/screens/MapScreen.js
+++ week7/locapic-part3/screens/MapScreen.js
@@ -1,14 +1,46 @@
-import { Image, StyleSheet } from 'react-native';
+import { useEffect } from 'react';
+import React from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
+import {Dimensions} from 'react-native';
+import { useState} from 'react';
+
+import * as Location from 'expo-location';
 
 export default function MapScreen() {
+  const [currentPosition, setCurrentPosition] = useState(null)
+
+  useEffect(() => {
+    (async () => {
+      const { status }
+       = await Location.requestForegroundPermissionsAsync();
+ 
+      if (status === 'granted') {
+        Location.watchPositionAsync({ distanceInterval: 10 },
+          (location) => {
+        setCurrentPosition(location.coords);
+      });
+
+    }
+  })();
+  }, []);
+  
   return (
-    <Image source={require('../assets/map.jpg')} style={styles.map} />
+    <View style={{flex:1}}>
+      <MapView mapType='hybrid' style={{ flex:1}}>
+        {currentPosition && <Marker coordinate={currentPosition} title ='My position'pinColor='#fecb2d' />}
+      </MapView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  map: {
-    width: '100%',
-    height: '100%',
+  container: {
+    flex: 1,
   },
+  map: {
+    width: Dimensions.get('window').width,
+    height:  Dimensions.get('window').height,
+  },
+
 });
