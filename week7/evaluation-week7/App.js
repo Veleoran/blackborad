90e933d21d7621af5b8bab8dbbb31602093e8388
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-05-05 13:27:29 +0200

    e

--- week7/evaluation-week7/App.js
+++ week7/evaluation-week7/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Text, View, TextInput, Pressable, SafeAreaView } from 'react-native';
+import { Text, View, TextInput, Pressable, SafeAreaView, StyleSheet } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
@@ -25,37 +25,40 @@ export default function App() {
   };
 
   return (
-    <View>
-      <SafeAreaView>
+    <View style={styles.container}>
+      <SafeAreaView style={styles.inputSection}>
         <TextInput
           placeholder='Place Name'
           value={name}
           onChangeText={setName}
+          style={styles.input}
         />
         <TextInput
           placeholder='Latitude'
           value={latitude}
           onChangeText={setLatitude}
           keyboardType='numeric'
+          style={styles.input}
         />
         <TextInput
           placeholder='Longitude'
           value={longitude}
           onChangeText={setLongitude}
           keyboardType='numeric'
+          style={styles.input}
         />
-        <Pressable onPress={addMarker}>
-          <Text>Go</Text>
+        <Pressable style={styles.button} onPress={addMarker}>
+          <Text style={styles.buttonText}>Go</Text>
         </Pressable>
       </SafeAreaView>
       <MapView
         initialRegion={{
-          latitude: 5,
-          longitude: 5,
-          latitudeDelta: 0.5,
-          longitudeDelta: 0.5,
+          latitude: 45, // Choisir une latitude qui inclut toutes vos villes
+          longitude: 0, // Choisir une longitude qui inclut toutes vos villes
+          latitudeDelta: 90,
+          longitudeDelta: 180,
         }}
-        style={{ width: '100%', height: '70%' }}>
+        style={styles.map}>
         {markers.map((marker, index) => (
           <Marker
             key={index}
@@ -68,43 +71,42 @@ export default function App() {
   );
 }
 
-
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'flex-end',
-		alignItems: 'center',
-	},
-	map: {
-		width: '100%',
-		height: '70%',
-	},
-	inputSection: {
-		width: '100%',
-		height: '30%',
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	input: {
-		borderWidth: 1,
-		width: '80%',
-		borderColor: 'gray',
-		paddingHorizontal: 20,
-		paddingVertical: 5,
-		margin: 5,
-		borderRadius: 10,
-	},
-	button: {
-		marginTop: 5,
-		paddingHorizontal: 12,
-		paddingVertical: 3,
-		borderRadius: 10,
-		backgroundColor: '#16C172',
-	},
-	buttonText: {
-		color: 'white',
-		fontSize: 17,
-		fontWeight: 'bold',
-		paddingHorizontal: 20,
-	},
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '70%',
+  },
+  inputSection: {
+    width: '100%',
+    height: '30%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    width: '80%',
+    borderColor: 'gray',
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    margin: 5,
+    borderRadius: 10,
+  },
+  button: {
+    marginTop: 5,
+    paddingHorizontal: 12,
+    paddingVertical: 3,
+    borderRadius: 10,
+    backgroundColor: '#16C172',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 17,
+    fontWeight: 'bold',
+    paddingHorizontal: 20,
+  },
 });
