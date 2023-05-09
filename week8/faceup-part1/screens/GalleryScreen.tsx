63fd60cc4c06fc5d3213355c420fb2e8ddb1cf2b
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-05-09 15:35:33 +0200

    e

--- week8/faceup-part1/screens/GalleryScreen.tsx
+++ week8/faceup-part1/screens/GalleryScreen.tsx
@@ -1,14 +1,54 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, FlatList, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+const photosData = [
+  require('../assets/picture1.jpg'),
+  require('../assets/picture2.jpg'),
+  require('../assets/picture3.jpg'),
+  require('../assets/picture4.jpg'),
+];
 
 export function GalleryScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Gallery Screen</Text>
-      <Image source={require('../assets/picture1.jpg')} />
-      <Image source={require('../assets/picture2.jpg')} />
-      <Image source={require('../assets/picture3.jpg')} />
-      <Image source={require('../assets/picture4.jpg')} />
+    <View style={styles.container}>
+      <FlatList
+        data={photosData}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.imageContainer}>
+            <Image source={item} style={styles.image} />
+            <TouchableOpacity style={styles.deleteIcon}>
+              <FontAwesome name="times" size={20} color="#FFF" />
+            </TouchableOpacity>
+          </View>
+        )}
+        numColumns={2}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    margin: 5,
+    position: 'relative',
+  },
+  image: {
+    width: '100%',
+    height: 150,
+    resizeMode: 'cover',
+  },
+  deleteIcon: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    padding: 5,
+  },
+});
--- week8/faceup-part1/screens/SnapScreen.tsx
+++ week8/faceup-part1/screens/SnapScreen.tsx
@@ -8,3 +8,10 @@ export function SnapScreen() {
     </View>
   );
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
