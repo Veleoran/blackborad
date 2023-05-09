c63dbd82cdfbef53802936dbd8da29acfedf3e72
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-05-09 12:32:43 +0200

    e

--- week8/faceup-part1/App.tsx
+++ week8/faceup-part1/App.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import HomeScreen from './screens/HomeScreen';
+import MGaleleryScreen from './screens/MapScreen';
+import PlacesScreen from './screens/PlacesScreen';
+
 
 export default function App() {
   return (
