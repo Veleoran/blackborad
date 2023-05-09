384d3d5f185024db345c40a81c5cc56c782e4c2d
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-05-09 13:36:21 +0200

    e

--- week8/faceup-part1/screens/HomeScreen.tsx
+++ week8/faceup-part1/screens/HomeScreen.tsx
@@ -6,6 +6,15 @@ const handleSubmit = () => {
  navigation.navigate('TabNavigator', { screen: 'Gallery' });
 };
 
+const EMAIL_REGEX: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const handleSubmit = () => {
+ if (EMAIL_REGEX.test(email)) {
+   navigation.navigate('TabNavigator', { screen: 'Gallery' });
+ } else {
+   setEmailError(true);
+ }
+};
 
 
 export function HomeScreen({ navigation }) {
