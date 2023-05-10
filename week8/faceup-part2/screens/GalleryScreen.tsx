5cc17d89499b809a96fb88c4c8525dbf3c020d2b
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-05-10 14:57:51 +0200

    e

--- week8/faceup-part2/screens/GalleryScreen.tsx
+++ week8/faceup-part2/screens/GalleryScreen.tsx
@@ -5,67 +5,38 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
-} from 'react-native';
-import { useSelector } from 'react-redux';
-import { UserState } from '../reducers/user';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-
-export default function GalleryScreen() {
+ } from 'react-native';
+ import { useDispatch, useSelector } from 'react-redux';
+ import { removePhoto, UserState } from '../reducers/user';
+ import FontAwesome from 'react-native-vector-icons/FontAwesome';
+ 
+ export default function GalleryScreen() {
+  const dispatch = useDispatch();
   const user = useSelector((state: { user: UserState }) => state.user.value);
-  const photosData =const photosData = useSelector(state => state.user.photos);
-
-
-  const photos = photosData.map((data: string, i: number) => {
+ 
+  const photos = user.photos.map((data: string, i: number) => {
     return (
       <View key={i} style={styles.photoContainer}>
-        <FontAwesome name='times' size={20} color='#000000' style={styles.deleteIcon} />
+        <TouchableOpacity onPress={() => dispatch(removePhoto(data))}>
+          <FontAwesome name='times' size={20} color='#000000' style={styles.deleteIcon} />
+        </TouchableOpacity>
+ 
         <Image source={{ uri: data }} style={styles.photo} />
       </View>
     );
   });
-
+ 
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Gallery</Text>
       <Text style={styles.text}>Logged as: {user.email}</Text>
-
+ 
       <ScrollView contentContainerStyle={styles.galleryContainer}>
         {photos}
       </ScrollView>
     </SafeAreaView>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-  },
-  galleryContainer: {
-    flexWrap: 'wrap',
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  photoContainer: {
-    alignItems: 'flex-end',
-  },
-  photo: {
-    margin: 10,
-    width: 150,
-    height: 150,
-  },
-  title: {
-    fontFamily: 'Futura',
-    fontSize: 22,
-    marginTop: 10,
-    marginBottom: 10,
-  },
-  deleteIcon: {
-    marginRight: 10,
-  },
-  text: {
-    marginBottom: 15,
-  },
-});
+ }
+ 
